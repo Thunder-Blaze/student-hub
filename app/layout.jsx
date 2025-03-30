2a3d4901de05947ece2f6f5398ed9f099e67705c
Om Swami <reach.oms2004@gmail.com>
2025-03-31 01:37:28 +0530

    lint check
    Signed-off-by: Om Swami <reach.oms2004@gmail.com>

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,13 +14,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={
+ className={
           inter.className + ' flex flex-col min-h-screen justify-between'
         }
       >
-        <Header />
+   <Header />
         {children}
-        <Footer />
+  <Footer />
       </body>
     </html>
   )
